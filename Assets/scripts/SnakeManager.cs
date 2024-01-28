5d90b8244c093e09e2b67b247510c7d2aafcbe44
ColinKintzinger <majic11300@gmail.com>
2024-01-27 21:03:58 -0600

    added movement and adding segments script

---
+++ Assets/scripts/SnakeManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static Unity.Burst.Intrinsics.X86.Avx;
+
+public class SnakeManager : MonoBehaviour
+{
+    [SerializeField] float distance = .2f;
+    [SerializeField] float speed = 100;
+    [SerializeField] float turnSpeed = 100;
+    [SerializeField] List<GameObject> segments= new List<GameObject>();
+    List<GameObject> snakebody = new List<GameObject>();
+    float count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        makeSegment();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(segments.Count > 0) 
+        {
+            makeSegment();
+        }
+        snakeMovement(); 
+    }
+    void snakeMovement()
+    {
+        //move the head with inputs
+        snakebody[0].GetComponent<Rigidbody2D>().velocity = snakebody[0].transform.right * speed * Time.deltaTime;
+        if(Input.GetAxis("Horizontal")!= 0)
+        {
+            snakebody[0].transform.Rotate(new Vector3(0,0,-turnSpeed*Time.deltaTime * Input.GetAxis("Horizontal")));
+        }
+    }
+    void makeSegment()
+    {
+        if (snakebody.Count == 0)
+        {
+            GameObject tmp1 = Instantiate(segments[0], transform.position, transform.rotation, transform);
+            if (!tmp1.GetComponent<markerManager>())
+                tmp1.AddComponent<markerManager>();
+            if (!tmp1.GetComponent<Rigidbody2D>())
+            {
+                tmp1.AddComponent<Rigidbody2D>();
+                tmp1.GetComponent<Rigidbody2D>().gravityScale = 0;
+            }
+            snakebody.Add(tmp1);
+            segments.RemoveAt(0);
+        }
+        markerManager markM = snakebody[snakebody.Count-1].GetComponent<markerManager>();
+        if(count ==0)
+        {
+            markM.clearMarkers();
+        }
+        count += Time.deltaTime;
+        if(count >= distance)
+        {
+        GameObject tmp = Instantiate(segments[0], markM.markerList[0].position , markM.markerList[0].rotation, transform);
+        if(!tmp.GetComponent<markerManager>())
+            tmp.AddComponent<markerManager>();
+        if(!tmp.GetComponent<Rigidbody2D>())
+        {
+            tmp.AddComponent<Rigidbody2D>();
+            tmp.GetComponent<Rigidbody2D>().gravityScale=0;
+        }
+        snakebody.Add(tmp);
+        segments.RemoveAt(0);
+        tmp.GetComponent<markerManager>().clearMarkers();
+            count = 0;
+        }
+    }
+}
---
+++ Assets/scripts/markerManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class markerManager : MonoBehaviour
+{
+    public class Marker { 
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public Marker(Vector3 pos, Quaternion rot)
+        {
+            position = pos;
+            rotation = rot;
+        }
+
+    }
+    public List<Marker> markerList = new List<Marker>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnDisconnectedFromMasterServer()
+    {
+        markerList.Add(new Marker(transform.position, transform.rotation));
+    }
+    public void clearMarkers()
+    {
+        markerList.Clear();
+        markerList.Add(new Marker(transform.position, transform.rotation)); 
+    }
+}
