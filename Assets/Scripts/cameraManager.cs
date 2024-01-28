684ff6aa3ac9eab48c0ac40e9da22f27c39cf994
APico23 <therealpico23@gmail.com>
2024-01-28 15:01:14 -0600

    Pretty Basic Camera Tracking

--- Assets/Scripts/cameraManager.cs
+++ Assets/Scripts/cameraManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class cameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] SnakeManager snakeManager;
+    Transform target;
+    [SerializeField] float smoothing;
+    [SerializeField] float initialCameraSize;
+    float cameraSize;
+    int lastSize = 0;
+
+    private void Start()
     {
-        
+        target = snakeManager.snakebody[0].transform;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void LateUpdate()
     {
-        
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+            if (lastSize != snakeManager.snakebody.Count)
+            {
+                lastSize = snakeManager.snakebody.Count;
+                cameraSize = initialCameraSize + (0.2f * snakeManager.snakebody.Count);
+                GetComponent<Camera>().orthographicSize = cameraSize;
+            }
+
+        }
     }
+
+
 }
