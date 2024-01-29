3ef9991ae824d9dc2862b2a6672f9914b5dc936b
Blake Klug <baklug9@gmail.com>
2024-01-28 22:14:59 -0600

    Added Title Screen

---
+++ Assets/scripts/Rotate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,.2f,0);
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
